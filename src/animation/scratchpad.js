1d2019c25f56f964a3eb2996eaacf4f6d84b5faf
MAIN\SK040122 <Tomas.Kosegi@anritsu.com>
2018-04-26 16:43:20 +0200

    ecperiment

---
+++ src/animation/scratchpad.js
@@ -0,0 +1,33 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+var start = null;
+var rID = null;
+var NF = 50;
+var f = 1;
+var duration = 2000
+
+function update(timestamp) {
+  if (!start) start = timestamp;
+  var progress = ((timestamp - start) / duration).toFixed(2);
+  
+  if (progress > 0.5) {
+    console.log('stop');
+  }
+  if (progress < 1) {
+    rID = requestAnimationFrame(update);
+  };
+  console.log(progress);
+  
+
+  
+
+}
+
+rID = requestAnimationFrame(update);
